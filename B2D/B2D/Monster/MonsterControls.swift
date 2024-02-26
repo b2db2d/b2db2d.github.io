c947de6625e7a663cb2a46c5cab848577090b815
heyou32 <heyou32@gmail.com>
2024-02-26 15:02:58 +0900

    delte button
    MonsterControls -> 버튼 삭제하면서 view 사용이 불필요해져서 Observable class 로 수정

--- B2D/B2D/Monster/MonsterControls.swift
+++ B2D/B2D/Monster/MonsterControls.swift
@@ -1,63 +1,49 @@
 import SwiftUI
 
-var timer: Timer? = nil
-struct MonsterControls: View {
-    @Environment(ViewModel.self) private var model
-    @State private var isPickerVisible: Bool = false
-
-    // MonsterEntity 클래스의 인스턴스를 옵셔널로 선언
-    @State private var monsterEntity: MonsterEntity?
+@Observable
+class MonsterControls {
+    private var model: ViewModel
+    private var isPickerVisible: Bool = false
     
-    @State private var change: Singularity? = Singularity.none
-
-    var body: some View {
-        @Bindable var model = model
-
-        VStack() {
-           
-            HStack(spacing: 17) {
-                Toggle(isOn: $isPickerVisible) {
-                    Label("Change", systemImage: "clock")
-                }
-            }
-            .toggleStyle(.button)
-            .buttonStyle(.borderless)
-            .labelStyle(.iconOnly)
-            .padding(12)
-            .glassBackgroundEffect(in: .rect(cornerRadius: 50))
-            .alignmentGuide(.controlPanelGuide) { context in
-                context[HorizontalAlignment.center]
-            }
-        } .onAppear {
-            timer?.invalidate()
-            
-            guard
-                model.inputTime.truncatingRemainder(dividingBy: 4) == 0 else{
-                // 초 단위는 없을 것 같아서 여길 들어올 일은 없을 거라 생각해서 일단 지금은 이렇게 사용하는 것으로..
-                print("Error: inputTime 이 이상함.( 4로 나눌 수 없음.)")
-                return
-            }
-            
-            timer = Timer.scheduledTimer(withTimeInterval: model.inputTime / 4 , repeats: true) { _ in
-                change = change?.next()
-                if let change = change {
-                    model.monsterChange = change
-                    // print("@@@@@" , change)
-                    model.monsterShadow.date = change.date
-                    Task {
-                        if monsterEntity == nil {
-                            monsterEntity = MonsterEntity()
-                        }
-                        await monsterEntity?.changeFunc(date: change.date)
+    // MonsterEntity 클래스의 인스턴스를 옵셔널로 선언
+    private var monsterEntity: MonsterEntity?
+    
+    private var change: Singularity? = Singularity.none
+    
+    var timer: Timer? = nil
+    
+    init(model:ViewModel){
+        self.model = model
+        self.timer?.invalidate()
+        
+        guard
+            model.inputTime.truncatingRemainder(dividingBy: 4) == 0 else{
+            // 초 단위는 없을 것 같아서 여길 들어올 일은 없을 거라 생각해서 일단 지금은 이렇게 사용하는 것으로..
+            print("Error: inputTime 이 이상함.( 4로 나눌 수 없음.)")
+            return
+        }
+        
+        timer = Timer.scheduledTimer(withTimeInterval: model.inputTime / 4 , repeats: true) { [weak self] _ in
+            guard let self = self else {return}
+            self.change = change?.next()
+            if let change = self.change {
+                self.model.monsterChange = change
+                print("@@@@@" , change)
+                self.model.monsterShadow.date = change.date
+                Task {
+                    if self.monsterEntity == nil {
+                        self.monsterEntity = await MonsterEntity()
                     }
-                } else {
-                    timer?.invalidate()
-                    timer = nil
+                    await self.monsterEntity?.changeFunc(date: change.date)
                 }
-                
+            } else {
+                self.timer?.invalidate()
+                self.timer = nil
             }
+            
         }
     }
+    
 }
 
 
@@ -67,7 +53,7 @@ struct MonsterControls: View {
 enum Singularity: String, CaseIterable, Identifiable {
     case none, march, june, september
     var id: Self { self }
-
+    
     var date: Date? {
         let month = switch self {
         case .none: 2
@@ -75,7 +61,7 @@ enum Singularity: String, CaseIterable, Identifiable {
         case .june: 6
         case .september: 10
         }
-
+        
         if month == 0 {
             return nil
         } else {
@@ -84,19 +70,15 @@ enum Singularity: String, CaseIterable, Identifiable {
     }
     
     func next() -> Singularity? {
-            let allCases = Singularity.allCases
-            let currentIndex = allCases.firstIndex(of: self)!
-            let nextIndex = allCases.index(after: currentIndex)
+        let allCases = Singularity.allCases
+        let currentIndex = allCases.firstIndex(of: self)!
+        let nextIndex = allCases.index(after: currentIndex)
         
-            if nextIndex == allCases.endIndex {
-                return nil
-            } else {
-                return allCases[nextIndex]
-            }
+        if nextIndex == allCases.endIndex {
+            return nil
+        } else {
+            return allCases[nextIndex]
         }
+    }
 }
 
-#Preview {
-    MonsterControls()
-        .environment(ViewModel())
-}
