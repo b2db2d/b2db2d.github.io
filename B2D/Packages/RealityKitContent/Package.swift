90f7ba2b5e34c4379e5aa54a03274a7e1b40d5c9
HeejinJo <midmost44@gmail.com>
2024-03-17 00:25:20 -0700

    segmentationFault11 error solved

--- B2D/Packages/RealityKitContent/Package.swift
+++ B2D/Packages/RealityKitContent/Package.swift
@@ -5,10 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "RealityKitContent",
-    // platforms check needed
-    platforms: [
-        .custom("xros", versionString: "1.0")
-    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
