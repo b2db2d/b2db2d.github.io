9ad12eb720f76b10c84c8a485a40e186954daef2
heyou32 <heyou32@gmail.com>
2024-04-16 15:28:28 +0900

    test fade in out
    opacity component 추가 (changFunc에서)

--- B2D/B2D/Entities/MonsterEntity.swift
+++ B2D/B2D/Entities/MonsterEntity.swift
@@ -19,7 +19,11 @@ class MonsterEntity: Entity {
         super.init()
         self.monster = monster
         
-        guard let entity = await RealityKitContent.entity(named: monster?.usdz_list[0] ?? "Scene9") else { return }
+        guard let monster = monster else {
+            print("Error: monster data missing  :MonsterEntity:init")
+            return
+        }
+        guard let entity = await RealityKitContent.entity(named: monster.usdz_list[0]) else { return }
         self.entity = entity
         self.addChild(entity)
         await update(
@@ -48,95 +52,90 @@ class MonsterEntity: Entity {
         let calendar = Calendar.autoupdatingCurrent
         _ = calendar.component(.month, from: date ?? Date())
         
-        do {
-            guard let usdzList = monster?.usdz_list, index < usdzList.count else {
-                print("Error: Index out of bounds or usdz_list is nil")
-                return
-            }
-            
-            guard let monsterName = monster?.usdz_list[index] else {
-                print("Error: \(monster), \(monster?.usdz_list[index])")
-                return
-            }
-            guard let newMonster = await RealityKitContent.entity(named: monsterName)else {
-                print("Error: failed load next monster \(monsterName)")
-                return
-            }
-            
-            //setOpacity(entity: newMonster, opacity: 0)
-            addChild(newMonster)
-            
-            if index+1 != monster?.usdz_list.count
-            {
-                if let animationKey = newMonster.availableAnimations.first {
-                    newMonster.playAnimation(animationKey.repeat(), transitionDuration: 0.3, startsPaused: false)
-                }
-            }
-            else{
-                if let animationKey = newMonster.availableAnimations.first {
-                    newMonster.playAnimation(animationKey , transitionDuration: 0.3, startsPaused: false)
-                }
-            }
-            
-            let fadeTime = 0.3
-            guard let curEntity = self.entity else { return }
-           // self.fadeIn(entity: self.entity!, duration: fadeTime)
-            //self.entity?.fadeOut(duration: fadeTime, targetOpacity: 0)
-            //DispatchQueue.main.asyncAfter(deadline: .now() + fadeTime){
-                
-            //}
-           // self.fadeOut(entity: newMonster, duration: fadeTime)
-            //newMonster.fadeIn(duration: fadeTime, targetOpacity: 1)
-           // DispatchQueue.main.asyncAfter(deadline: .now() + fadeTime){
-                self.entity?.removeFromParent()
-                self.entity = newMonster
-            //}
-            
-            index += 1
-            
-        } catch {
-            print("Error: \(monster?.usdz_list[index]) is not exist")
+        guard let monster = self.monster else{
+            print("Error: monster data missing  :MonsterEntity:changeFunc")
+            return
         }
+        if index >= monster.usdz_list.count {
+            print("Error: Index out of bounds or usdz_list is nil")
+            return
+        }
+        
+        let monsterName = monster.usdz_list[index]
+        
+        guard let newMonster = await RealityKitContent.entity(named: monsterName)else {
+            print("Error: failed load next monster \(monsterName)")
+            return
+        }
+        
+        let fadeTime = 0.5
+        
+        guard let curEntity = self.entity else {
+            return
+        }
+        if index == 0 {
+            if let animationKey = curEntity.availableAnimations.first {
+                curEntity.playAnimation(animationKey.repeat(), transitionDuration: 0.3, startsPaused: false)
+            }
+        } else {
+            self.fadeOut(entity: curEntity, duration: fadeTime)
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + fadeTime) {
+                self.setOpacity(entity: newMonster, opacity: 0)
+                self.addChild(newMonster)
+                if let animationKey = newMonster.availableAnimations.first {
+                    newMonster.playAnimation(animationKey, transitionDuration: 0.3, startsPaused: false)
+                }
+                self.fadeIn(entity: newMonster, duration: fadeTime)
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + fadeTime) {
+                    curEntity.removeFromParent()
+                    self.entity = newMonster
+                }
+            }
+        }
+        index += 1
     }
     
     // Entity Fade in / out
     func setOpacity(entity: Entity, opacity: Float) {
-        if let modelComponent = entity.components[ModelComponent.self] {
-            var newMaterials: [RealityKit.Material] = []
-            for material in modelComponent.materials {
-                var newMaterial = material
-                switch newMaterial {
-                case var simpleMaterial as SimpleMaterial:
-                    simpleMaterial.color = .init(tint: UIColor(white: 1.0, alpha: CGFloat(opacity)),texture: .none)
-                    newMaterial = simpleMaterial
-                default:
-                    break
-                }
-                newMaterials.append(newMaterial)
-            }
-            entity.components[ModelComponent.self]?.materials = newMaterials
-        }
+        //        if var opacityComponent = entity.components[OpacityComponent.self] {
+        //            print(opacityComponent.opacity)
+        //            opacityComponent.opacity = opacity
+        //            print(opacityComponent.opacity)
+        //        } else {
+        entity.components.set(OpacityComponent(opacity: opacity) )
+        //}
     }
-
+    
     func fadeIn(entity: Entity, duration: Double){
         let steps = Int(duration*100)
-           let timeInterval = duration / Double(steps)
-           for step in 1...steps {
-               DispatchQueue.main.asyncAfter(deadline: .now() + timeInterval * Double(step)) {
-                   let opacity = Float(step) / Float(steps)
-                   self.setOpacity(entity: entity, opacity: opacity)
-               }
-           }
+        let timeInterval = duration / Double(steps)
+        for step in 1...steps {
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + timeInterval * Double(step)) {
+                if step == steps {
+                    self.setOpacity(entity: entity, opacity: 1)
+                    return
+                }
+                let opacity = Float(step) / Float(steps)
+                self.setOpacity(entity: entity, opacity: opacity)
+            }
+        }
     }
     func fadeOut(entity: Entity, duration: Double){
         let steps = Int(duration*100)
-           let timeInterval = duration / Double(steps)
-           for step in 1...steps {
-               DispatchQueue.main.asyncAfter(deadline: .now() + timeInterval * Double(step)) {
-                   let opacity = 1 - Float(step) / Float(steps)
-                   self.setOpacity(entity: entity, opacity: opacity)
-               }
-           }
+        let timeInterval = duration / Double(steps)
+        for step in 1...steps {
+            DispatchQueue.main.asyncAfter(deadline: .now() + timeInterval * Double(step)) {
+                if step == steps {
+                    self.setOpacity(entity: entity, opacity: 0)
+                    return
+                }
+                let opacity = 1 - Float(step) / Float(steps)
+                self.setOpacity(entity: entity, opacity: opacity)
+            }
+        }
     }
 }
 
