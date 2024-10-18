ee1842d069201c026e22a456f0aef1ce4f51f171
Birth2Death <momo@Birth2Deaths-MacBook-Pro.local>
2024-10-18 15:04:24 -0700

    App icon and scale position setup

--- B2D/B2D/Entities/Entity+Configuration.swift
+++ B2D/B2D/Entities/Entity+Configuration.swift
@@ -3,8 +3,8 @@ import SwiftUI
 extension MonsterEntity {
     /// Configuration information for Monster entities.
     struct Configuration {
-        var scale: Float = 1
-        var position: SIMD3<Float> = .init(x: 0, y: 1, z: -0.8)
+        var scale: Float = 2.5
+        var position: SIMD3<Float> = .init(x: 0, y: 0.5, z: -1.5)
         var speed: Float = 0
         var isPaused: Bool = false
         var date: Date? = nil
